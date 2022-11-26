b7492f12da3fd9a18e683054f0de7b014408da7b
Heru Suseno <herususeno56@gmail.com>
2022-11-26 21:48:32 +0700

    kedua

--- config/database.js
+++ config/database.js
@@ -1,21 +1,9 @@
-// const mysql = require('mysql');
-// const koneksi = mysql.createConnection({
-//     host: 'localhost',
-//     user:'root',
-//     password:'',
-//     database:'api_nodejs',
-//     multipleStatements: true
-// });
-
-// koneksi.connect((err) =>{
-//     if(err) throw err;
-//     console.log('MySQL Connected');
-// })
-//  
-
-const { Sequelize } = require('sequelize');
- const koneksi = new Sequelize('api_nodejs', 'root', '', {
-    host: 'localhost',
-    dialect:  'mysql'
-  });
-  module.exports = koneksi;
+const { Sequelize } = require("sequelize");
+const { MYSQLDATABASE, MYSQLHOST, MYSQLPASSWORD, MYSQLPORT, MYSQLUSER } =
+  process.env;
+const koneksi = new Sequelize(MYSQLDATABASE, MYSQLUSER, MYSQLPASSWORD, {
+  host: MYSQLHOST,
+  dialect: "mysql",
+  port: MYSQLPORT,
+});
+module.exports = koneksi;
